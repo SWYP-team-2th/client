fbaee790e28286f4aee59283ad6eb9fa869475ba
YOOJS1205 <junsang8921@gmail.com>
2025-01-29 02:54:55 +0900

    의존성 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 

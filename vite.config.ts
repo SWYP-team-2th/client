a490e69c4cce08e308228ebb3497ca8093fb0b94
유준상 <89122773+YOOJS1205@users.noreply.github.com>
2025-01-31 21:53:49 +0900

    svg를 React Component로 사용할 수 있도록 설정한다. (#36)
    * svg를 React Component로 사용할 수 있도록 설정한다.
    
    * 예시 제거
    
    * package.json 캐시도 따라가도록 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },

8517934670950d5e638e402dbaca339875480471
정찬영 <q135794268020@gmail.com>
2025-02-18 01:30:47 +0900

    bgColor 제거

--- src/components/common/layout/SubLayout.tsx
+++ src/components/common/layout/SubLayout.tsx
@@ -1,5 +1,5 @@
 export default function SubLayout() {
   return (
-    <div className="w-full h-full mx-auto my-0 min-h-lvh border-x-[1px] desktop:w-[480px] bg-gray-100"></div>
+    <div className="w-full h-full mx-auto my-0 min-h-lvh border-x-[1px] desktop:w-[480px]"></div>
   );
 }

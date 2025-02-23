e4b0895538969343d3d93f724b1aabbdb1da44dd
YOOJS1205 <junsang8921@gmail.com>
2025-02-23 20:57:53 +0900

    마이페이지를 구현한다.

--- src/components/common/Loading/Loading.tsx
+++ src/components/common/Loading/Loading.tsx
@@ -1,11 +1,7 @@
-import Overlay from '@/components/common/Overlay';
-
 export default function Loading() {
   return (
-    <Overlay className="bg-gray-200">
-      <div className="flex items-center justify-center h-full">
-        <div className="w-10 h-10 border-t-2 border-b-2 border-primary-500 rounded-full animate-spin"></div>
-      </div>
-    </Overlay>
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="w-10 h-10 border-t-2 border-b-2 border-primary-500 rounded-full animate-spin"></div>
+    </div>
   );
 }

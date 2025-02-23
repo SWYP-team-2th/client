b60e2c0847cb3cf33b4f74bc17f0699d8cb31adf
YOOJS1205 <junsang8921@gmail.com>
2025-02-23 17:42:47 +0900

    delete console

--- src/components/vote-regist/VoteDescription/hooks.ts
+++ src/components/vote-regist/VoteDescription/hooks.ts
@@ -3,8 +3,6 @@ import useVoteRegist from '../Provider/hooks';
 export default function useVoteDescription() {
   const { state, setDescription } = useVoteRegist();
 
-  console.log(state);
-
   const description = state.description.value;
 
   return {

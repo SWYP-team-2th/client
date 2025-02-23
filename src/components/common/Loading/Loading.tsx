f4caf691e296f45e81b04b748bc9cebc1c9c5de5
YOOJS1205 <junsang8921@gmail.com>
2025-02-23 23:26:47 +0900

    spinner로 변경

--- src/components/common/Loading/Loading.tsx
+++ src/components/common/Loading/Loading.tsx
@@ -1,7 +1,9 @@
+import Spinner from '@/assets/gif/spinner.gif';
+
 export default function Loading() {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <div className="w-10 h-10 border-t-2 border-b-2 border-primary-500 rounded-full animate-spin"></div>
+      <img src={Spinner} alt="loading" className="w-10 h-10" />
     </div>
   );
 }

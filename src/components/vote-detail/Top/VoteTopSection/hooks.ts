dc54143a5087305db1fb345340d62fe7f4af22a6
정찬영 <q135794268020@gmail.com>
2025-02-26 18:47:30 +0900

    서버 request값 변경으로 인한 파일 삭제

--- src/components/vote-detail/Top/VoteTopSection/hooks.ts
+++
@@ -1,8 +0,0 @@
-import useGetVoteDetail from '@/api/useGetVoteDetail';
-
-export default function useVoteDetail() {
-  const { data: topInfo } = useGetVoteDetail('1');
-  return {
-    topInfo,
-  };
-}

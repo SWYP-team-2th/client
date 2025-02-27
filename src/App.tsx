085178b2607a90cb784ce5901e4a85b7f3cfba55
YOOJS1205 <junsang8921@gmail.com>
2025-02-27 20:40:17 +0900

    테스트용 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import LinkShareBottomSheet from './components/common/LinkShareBottomSheet';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <LinkShareBottomSheet
+        author="김가림"
+        shareUrl="https://dev.photopic.site"
+      />
+    </>
+  );
 }
 
 export default App;

8aed6f888138ac962aeb82b7a34111f45236492b
YOOJS1205 <junsang8921@gmail.com>
2025-02-28 23:43:35 +0900

    투표 마감, 삭제 처리한다.

--- src/api/useGetIdentificationInfo.ts
+++ src/api/useGetIdentificationInfo.ts
@@ -1,27 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
 import { request } from '@/api/config';
-import { getAccessToken } from '@/components/login/Auth/token';
 
-interface IdentificationInfoResponse {
+interface MyInfoResponse {
   id: number;
   nickname: string;
   profileUrl: string;
 }
 
-export default function useGetIdentificationInfo() {
-  return useQuery<IdentificationInfoResponse>({
-    queryKey: ['identificationInfo'],
+export default function useGetMyInfo() {
+  return useQuery<MyInfoResponse>({
+    queryKey: ['my-info'],
     queryFn: async () => {
-      const token = getAccessToken();
-      return request<IdentificationInfoResponse>({
+      return request<MyInfoResponse>({
         method: 'GET',
         url: '/users/me',
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
       });
     },
-    // 토큰이 저장되도 여기 쿼리문이 실행되지 않기 때문에 토큰이 들어오면 현재 쿼리문 실행되도록 설정
-    enabled: !!getAccessToken(),
   });
 }

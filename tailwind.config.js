ad3529e53a2e7160d257cf3aaa82442f883f4771
유준상 <89122773+YOOJS1205@users.noreply.github.com>
2025-03-03 20:03:44 +0900

    게스트 로그인 로직 전반 수정, 다이얼로그 전반 수정 및 반영 (#193)

--- tailwind.config.js
+++ tailwind.config.js
@@ -85,14 +85,6 @@ export const theme = {
         '11px',
         { lineHeight: '1.2', letterSpacing: '-0.02em', fontWeight: '400' },
       ],
-      'label-x-small-1': [
-        '11px',
-        { lineHeight: '1.2', letterSpacing: '-0.02em', fontWeight: 'bold' },
-      ],
-      'label-x-small-2': [
-        '11px',
-        { lineHeight: '1.2', letterSpacing: '-0.02em', fontWeight: '400' },
-      ],
     },
   },
 };

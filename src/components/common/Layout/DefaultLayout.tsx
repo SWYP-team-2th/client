23bc2c6ae4eb4614d569badf261e431027f68583
정찬영 <q135794268020@gmail.com>
2025-03-06 19:35:42 +0900

    홈페이지 라우팅 추가, 바텀 네비게이션 불필요 routing 설정, defaultLayout 플로팅 버튼 삭제

--- src/components/common/Layout/DefaultLayout.tsx
+++ src/components/common/Layout/DefaultLayout.tsx
@@ -1,44 +1,11 @@
-import { useNavigate, Outlet } from 'react-router-dom';
-import LoginDialog from '../LoginDialog';
-import useGetMyInfo from '@/api/useGetMyInfo';
-import { FloatingButton } from '@/components/common/Button/FloatingButton';
-import { useDialog } from '@/components/common/Dialog/hooks';
-import GuestConfirmDialog from '@/components/common/GuestConfirmDialog/GuestConfirmDialog';
-import Icon from '@/components/common/Icon';
-import { getRole } from '@/components/login/Auth/token';
+import { Outlet } from 'react-router-dom';
+import Navigation from '@/components/common/Navigation/Navigation';
 
 export default function DefaultLayout() {
-  const navigate = useNavigate();
-  const { openDialog } = useDialog();
-  const { data: myInfo } = useGetMyInfo();
-
-  const handleClickFloatingButton = () => {
-    if (!myInfo) {
-      openDialog(<LoginDialog />);
-      return;
-    }
-
-    if (myInfo && getRole() === 'GUEST') {
-      openDialog(
-        <GuestConfirmDialog title="게스트 계정은 투표를 올릴 수 없어요 😢" />,
-      );
-      return;
-    }
-
-    navigate('/votes/regist');
-  };
-
   return (
     <div className="relative w-full h-full mx-auto my-0 min-h-lvh max-w-[480px]">
       <Outlet />
-      <FloatingButton
-        onClick={handleClickFloatingButton}
-        className="absolute bottom-25 right-6 z-50"
-        buttonType="secondary"
-        size="large"
-      >
-        <Icon name="PlusWhite" size="large" />
-      </FloatingButton>
+      <Navigation />
     </div>
   );
 }

b43c62bd8faddd7ce818e4b8e3fe49cceb3dab34
YOOJS1205 <junsang8921@gmail.com>
2025-03-08 02:52:07 +0900

    이미지 상세 모달 2차 MVP 반영한다.

--- src/components/vote-detail/ImageDetailModal/hooks.ts
+++ src/components/vote-detail/ImageDetailModal/hooks.ts
@@ -1,35 +1,69 @@
+import { useQueryClient } from '@tanstack/react-query';
 import { useEffect, useRef, useState } from 'react';
-import { Image } from '@/api/useGetVoteDetail';
+import useGetVoteDetail from '@/api/useGetVoteDetail';
+import useVote from '@/api/useVote';
 import { useDialog } from '@/components/common/Dialog/hooks';
 
 interface UseImageDetailModalOptions {
-  images: Image[];
   selectedImageId: number;
 }
 
 export default function useImageDetailModal({
-  images,
   selectedImageId,
 }: UseImageDetailModalOptions) {
+  const queryClient = useQueryClient();
   const { closeDialog } = useDialog();
+  const shareUrl = window.location.pathname.split('/')[2];
+  const { data: voteDetail } = useGetVoteDetail(shareUrl);
+  const { mutate: postVote, isPending: isPostVotePending } = useVote(
+    voteDetail?.id ?? 0,
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: ['voteDetail', shareUrl],
+        });
+      },
+    },
+  );
 
   const [currentImageId, setCurrentImageId] = useState(
-    selectedImageId || images[0]?.id,
+    (selectedImageId || voteDetail?.images[0]?.id) ?? 0,
   );
 
   const scrollContainerRef = useRef<HTMLDivElement>(null);
 
-  const currentIndex = images.findIndex((img) => img.id === currentImageId);
+  const currentIndex =
+    voteDetail?.images.findIndex((img) => img.id === currentImageId) ?? 0;
 
-  const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+  const handleScrollCapture = (e: React.UIEvent<HTMLDivElement>) => {
     const container = e.currentTarget;
     const itemWidth = container.clientWidth;
 
     const visibleIndex = Math.round(container.scrollLeft / itemWidth);
 
-    if (images[visibleIndex] && images[visibleIndex].id !== currentImageId) {
-      setCurrentImageId(images[visibleIndex].id);
+    if (
+      voteDetail?.images[visibleIndex] &&
+      voteDetail?.images[visibleIndex].id !== currentImageId
+    ) {
+      setCurrentImageId(voteDetail?.images[visibleIndex].id ?? 0);
     }
+  };
+
+  const handleClickImage = (id: number) => {
+    const index = voteDetail?.images.findIndex((img) => img.id === id) ?? 0;
+
+    if (scrollContainerRef.current && index !== -1) {
+      scrollContainerRef.current.scrollLeft =
+        index * scrollContainerRef.current.clientWidth;
+
+      setTimeout(() => {
+        setCurrentImageId(id);
+      }, 300);
+    }
+  };
+
+  const handleClickVoteButton = () => {
+    postVote(currentImageId);
   };
 
   useEffect(() => {
@@ -41,10 +75,13 @@ export default function useImageDetailModal({
 
   return {
     scrollContainerRef,
-    imageNum: images.length,
-    images,
+    images: voteDetail?.images ?? [],
+    isPostVotePending,
     currentIndex,
-    handleScroll,
+    currentImageId,
+    handleScrollCapture,
+    handleClickImage,
+    handleClickVoteButton,
     closeDialog,
   };
 }

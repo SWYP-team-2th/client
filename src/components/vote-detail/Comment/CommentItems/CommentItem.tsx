63dd189a1d74740edcae516901047b9b06c24dc0
정찬영 <q135794268020@gmail.com>
2025-03-09 18:54:11 +0900

    수정 삭제 아이콘 적용, 댓글 유저 이미지 위치 수정

--- src/components/vote-detail/Comment/CommentItems/CommentItem.tsx
+++ src/components/vote-detail/Comment/CommentItems/CommentItem.tsx
@@ -1,3 +1,5 @@
+import Icon from '@/components/common/Icon';
+
 interface AuthorType {
   userId: number;
   nickname: string;
@@ -18,17 +20,25 @@ interface CommentItemProps {
 
 export default function CommentItem({ comment }: CommentItemProps) {
   return (
-    <div key={comment.commentId} className="flex mb-3 ">
-      <div className="flex ">
-        <img
-          src={comment.author.profileUrl}
-          className="w-[20px] h-[20px] rounded-full"
-          alt="프로필 이미지"
-        />
-      </div>
-      <div className="flex flex-col ml-[6px]">
+    <div key={comment.commentId} className="flex mb-3">
+      <img
+        src={comment.author.profileUrl}
+        className="w-7 h-7 rounded-full mt-[2px]"
+        alt="프로필 이미지"
+      />
+
+      <div className="flex flex-col ml-[6px] flex-grow">
         <span className="text-label-medium">{comment.author.nickname}</span>
         <span className="break-words text-body-2-long">{comment.content}</span>
+      </div>
+
+      <div className="space-x-2">
+        <button className="cursor-pointer">
+          <Icon name="PencilGray" size="extra-small" />
+        </button>
+        <button className="cursor-pointer">
+          <Icon name="TrashCanGray" size="extra-small" />
+        </button>
       </div>
     </div>
   );

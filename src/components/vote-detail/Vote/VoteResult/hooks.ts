e47ed39011428257fdbc8707892101e13e546f4d
정찬영 <q135794268020@gmail.com>
2025-03-11 13:06:30 +0900

    주석 제거

--- src/components/vote-detail/Vote/VoteResult/hooks.ts
+++ src/components/vote-detail/Vote/VoteResult/hooks.ts
@@ -12,12 +12,11 @@ export default function useVoteStatus({ shareUrl }: UseVoteStatusOptions) {
     enabled: !!voteDetail.id,
   });
 
-  // ✅ 유저가 투표했는지 여부 확인 (images 배열 중 voted: true가 있는지 체크)
   const userHasVoted =
     voteDetail?.images?.some((image) => image.voteId) ?? false;
 
   return {
     voteStatus,
-    userHasVoted, // ✅ 유저 투표 여부 추가
+    userHasVoted,
   };
 }

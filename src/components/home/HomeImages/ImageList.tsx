725746271233626244a9177cd8447c7e0e2f0555
정찬영 <q135794268020@gmail.com>
2025-03-11 23:06:12 +0900

    홈에서 게시글의 이미지 클릭 시 해당 게시글 상세페이지로 이동

--- src/components/home/HomeImages/ImageList.tsx
+++ src/components/home/HomeImages/ImageList.tsx
@@ -1,17 +1,31 @@
+import { useNavigate } from 'react-router-dom';
 import ImageItem from '@/components/home/HomeImages/ImageItem';
+import useGetVoteDetail from '@/api/useGetVoteDetail';
 
 interface ImageListProps {
   images: { id: number; imageUrl: string }[];
   shareUrl: string;
 }
 
-// 여기서 클릭 시 shareUrl 연결
-export default function ImageList({ images }: ImageListProps) {
+export default function ImageList({ images, shareUrl }: ImageListProps) {
+  const navigate = useNavigate();
+  const { data: voteDetail } = useGetVoteDetail(shareUrl);
+
+  const handleClick = () => {
+    if (voteDetail) {
+      navigate(`/votes/${voteDetail.shareUrl}`);
+    }
+  };
+
   return (
     <div className="w-full overflow-x-auto mb-[6px] pl-[2px]">
       <div className="flex space-x-[6px] w-max">
         {images.map((image) => (
-          <ImageItem key={image.id} imageUrl={image.imageUrl} />
+          <ImageItem
+            key={image.id}
+            imageUrl={image.imageUrl}
+            onClick={handleClick}
+          />
         ))}
       </div>
     </div>

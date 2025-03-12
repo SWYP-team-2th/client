44063b12e4f381586924c0a9109637e1048a9b8e
유준상 <89122773+YOOJS1205@users.noreply.github.com>
2025-03-12 18:23:17 +0900

    바텀 네비게이션이 컨텐츠 가리는 문제를 해결한다. (#267)

--- src/components/common/Layout/DefaultLayout.tsx
+++ src/components/common/Layout/DefaultLayout.tsx
@@ -1,11 +1,12 @@
-
 import { Outlet } from 'react-router-dom';
 import Navigation from '@/components/common/Navigation/Navigation';
 
 export default function DefaultLayout() {
   return (
     <div className="relative w-full h-full mx-auto my-0 min-h-lvh max-w-[480px]">
-      <Outlet />
+      <div className="pb-[80px]">
+        <Outlet />
+      </div>
       <Navigation />
     </div>
   );

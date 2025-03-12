4edfe6ff37c56fa68b9363f752c2e93cd017b2a8
유준상 <89122773+YOOJS1205@users.noreply.github.com>
2025-03-12 18:21:59 +0900

    댓글 삭제 후 바로 반영되지 않는 문제를 해결한다. (#257)

--- src/components/vote-detail/Comment/DeleteCommentDialog/hooks.ts
+++ src/components/vote-detail/Comment/DeleteCommentDialog/hooks.ts
@@ -1,12 +1,18 @@
+import { useQueryClient } from '@tanstack/react-query';
 import { useDeleteComment } from '@/api/useDeleteComment';
 import { useDialog } from '@/components/common/Dialog/hooks';
 
 export default function useDeleteCommentDialog() {
   const { closeDialog } = useDialog();
+  const queryClient = useQueryClient();
 
   const { mutate: deleteComment, isPending: isDeleteCommentPending } =
     useDeleteComment({
-      onSuccess: () => {
+      onSuccess: (_, { postId }) => {
+        queryClient.invalidateQueries({
+          queryKey: ['comments', postId],
+          exact: false,
+        });
         closeDialog();
       },
     });

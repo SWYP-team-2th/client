1b167879656e709582e9bd87a6d9c8cddc065583
YOOJS1205 <junsang8921@gmail.com>
2025-03-12 15:17:48 +0900

    댓글 삭제 후 바로 반영되지 않는 문제를 해결한다.

--- src/api/useDeleteComment.ts
+++ src/api/useDeleteComment.ts
@@ -1,8 +1,4 @@
-import {
-  useMutation,
-  useQueryClient,
-  UseMutationOptions,
-} from '@tanstack/react-query';
+import { useMutation, UseMutationOptions } from '@tanstack/react-query';
 import { request } from '@/api/config';
 
 interface DeleteCommentType {
@@ -13,17 +9,12 @@ interface DeleteCommentType {
 export function useDeleteComment(
   options?: UseMutationOptions<void, Error, DeleteCommentType>,
 ) {
-  const queryClient = useQueryClient();
-
   return useMutation<void, Error, DeleteCommentType>({
     mutationFn: ({ postId, commentId }) =>
       request({
         method: 'DELETE',
         url: `/posts/${postId}/comments/${commentId}`,
       }),
-    onSuccess: (_, { postId }) => {
-      queryClient.invalidateQueries({ queryKey: ['comments', postId] });
-    },
     ...options,
   });
 }

0d9df75fe29261430e5678c5553ad66753e4aec3
정찬영 <q135794268020@gmail.com>
2025-03-14 11:24:43 +0900

    투표 현황바 UI 개선

--- src/components/vote-detail/Vote/VoteResult/VoteResultItem.tsx
+++ src/components/vote-detail/Vote/VoteResult/VoteResultItem.tsx
@@ -13,16 +13,16 @@ export default function VoteResultItem({
 
   return (
     <div className="flex items-center mb-[6px]">
-      <div className="flex w-[30px]">
+      <div className="flex w-[40px]">
         <span className="text-label-small">{status.imageName}</span>
       </div>
-      <div className="ml-[9px] flex flex-1">
+      <div className="flex flex-1">
         <div
           className={`h-[13px] ${isHighest ? 'bg-primary-500' : 'bg-primary-300'} rounded-tr-[99px] rounded-br-[99px]`}
           style={{ width: `${voteRatio}%` }}
         />
       </div>
-      <div className="ml-[13px] flex">
+      <div className="ml-3 flex">
         <span className="text-gray-800 text-label-small">{`${status.voteCount}표`}</span>
         <span className="pl-[2px] text-gray-800 text-label-x-small-2">{`(${voteRatio}%)`}</span>
       </div>

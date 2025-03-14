b512bd907226b05bc48f5e61dfa5fc3b06d5f6e2
YOOJS1205 <junsang8921@gmail.com>
2025-03-14 20:34:59 +0900

    fix build failed

--- src/components/my/Profile/VoteList/MyVoteList/hooks.tsx
+++ src/components/my/Profile/VoteList/MyVoteList/hooks.tsx
@@ -1,21 +1,14 @@
-import { useQueryClient } from '@tanstack/react-query';
 import { useEffect, useRef } from 'react';
-import { useParams } from 'react-router-dom';
-import { UserInfoType } from '@/api/useGetUserInfo';
 import { useGetMyVoteList } from '@/api/useGetVoteList';
 import { useBottomSheet } from '@/components/common/BottomSheet/hooks';
 import LinkShareBottomSheet from '@/components/common/LinkShareBottomSheet';
 
 export default function useMyVoteList() {
-  const { userId } = useParams<{ userId: string }>();
   const observerRef = useRef<HTMLDivElement>(null);
   const { openBottomSheet } = useBottomSheet();
-  const queryClient = useQueryClient();
 
   const { data, hasNextPage, isFetchingNextPage, fetchNextPage } =
     useGetMyVoteList();
-
-  const userInfo = queryClient.getQueryData<UserInfoType>(['user', userId]);
 
   const myVoteList = data?.pages.flatMap((page) => page.data) ?? [];
 
@@ -46,7 +39,6 @@ export default function useMyVoteList() {
     openBottomSheet(<LinkShareBottomSheet shareUrl={shareUrl} />);
   };
   return {
-    author: userInfo?.nickname,
     myVoteList,
     observerRef,
     handleClickVoteShare,

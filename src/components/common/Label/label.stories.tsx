3f6ad5f406b88b5549d3c25a8ec0408fa6216329
정찬영 <q135794268020@gmail.com>
2025-06-03 02:50:00 +0900

    Label 컴포넌트 수정 (#332)
    * Label 수정사항 반영
    
    * lint 에러 수정

---
+++ src/components/common/Label/label.stories.tsx
@@ -0,0 +1,37 @@
+import { Label } from './Label';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Label> = {
+  title: 'common/Label',
+  component: Label,
+  argTypes: {
+    variant: {
+      control: { type: 'radio' },
+      options: ['solid', 'outline'],
+    },
+    colorVarient: {
+      control: { type: 'radio' },
+      options: ['neutral', 'progress', 'ended'],
+    },
+    size: {
+      control: { type: 'radio' },
+      options: ['small', 'medium'],
+    },
+    children: {
+      control: 'text',
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Label>;
+
+export const Example: Story = {
+  args: {
+    variant: 'solid',
+    colorVarient: 'neutral',
+    size: 'medium',
+    children: 'Label',
+  },
+};

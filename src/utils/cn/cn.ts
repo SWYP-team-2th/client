f789192efc8951da546c02ff38c0cc6080191ce7
유준상 (프론트) <89122773+YOOJS1205@users.noreply.github.com>
2025-06-02 18:43:39 +0900

    cn 유틸함수에서 text- 스타일이 오버라이딩되는 문제를 해결한다. (#330)

--- src/utils/cn/cn.ts
+++ src/utils/cn/cn.ts
@@ -1,5 +1,32 @@
 import { clsx, ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { extendTailwindMerge } from 'tailwind-merge';
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': [
+        'text-title-1',
+        'text-title-2',
+        'text-title-3',
+        'text-heading-1',
+        'text-heading-2',
+        'text-headline-1',
+        'text-headline-2',
+        'text-headline-3',
+        'text-body-1',
+        'text-body-1-long',
+        'text-body-2',
+        'text-body-2-long',
+        'text-caption-1',
+        'text-caption-2',
+        'text-label-1',
+        'text-label-2',
+        'text-label-3',
+        'text-label-4',
+      ],
+    },
+  },
+});
 
 const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 export default cn;

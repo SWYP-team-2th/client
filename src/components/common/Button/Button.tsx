9584fc4cda9f982ac12e67f91a6538dbac56327c
정찬영 <q135794268020@gmail.com>
2025-06-03 00:26:31 +0900

    빌드 꺠짐 문제 해결

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -15,21 +15,25 @@ const buttonVariants = cva(
         small: 'w-[160px] h-[40px] rounded-lg text-label-1',
         medium: 'w-[210px] h-[46px] rounded-xl text-headline-3',
         large: 'w-full h-[58px] rounded-2xl text-heading-2',
+        jumbo: 'w-full h-[1px]',
       },
       solidType: {
         primary: 'bg-primary-500 text-gray-100',
         secondary: 'bg-primary-700 text-gray-100',
         disabled: 'bg-gray-400 text-gray-100',
+        tertiary: 'bg-gray-100',
       },
       outlineType: {
         primary: 'border-primary-500 text-primary-600',
         secondary: 'border-primary-700 text-primary-700',
         disabled: 'border-gray-500 text-gray-600',
+        tertiary: 'bg-gray-100',
       },
       borderSize: {
         small: 'border-[1.5px]',
         medium: 'border-[1.8px]',
         large: 'border-[2px]',
+        jumbo: 'border-[1px]',
       },
     },
   },
@@ -39,7 +43,7 @@ interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
-  buttonType: 'primary' | 'secondary' | 'disabled';
+  buttonType: 'primary' | 'secondary' | 'disabled' | 'tertiary';
 }
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(

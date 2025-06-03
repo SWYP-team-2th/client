83f3d71944a3fb442f29fd740579a6648393763d
유준상 (프론트) <89122773+YOOJS1205@users.noreply.github.com>
2025-06-03 17:10:40 +0900

    투표제작페이지 - 투표 설정 섹션제작 (#336)

---
+++ src/components/vote-regist/PollOptionSection/PollOptionSection.tsx
@@ -0,0 +1,25 @@
+import FieldContainer from '../FieldContainer';
+import usePollOptionSection from './hooks';
+import Switch from '@/components/common/Switch';
+
+export default function PollOptionSection() {
+  const { POLL_OPTIONS } = usePollOptionSection();
+
+  return (
+    <FieldContainer title="투표 설정" className="flex flex-col gap-5">
+      {POLL_OPTIONS.map((option) => (
+        <div
+          key={option.value}
+          className="flex items-center justify-between text-gray-900 text-headline-2"
+        >
+          <p>{option.label}</p>
+          <Switch
+            size="medium"
+            checked={option.checked}
+            onChange={(value) => option.onChange(value)}
+          />
+        </div>
+      ))}
+    </FieldContainer>
+  );
+}
--- src/pages/Vote/VoteRegistPage.tsx
+++ src/pages/Vote/VoteRegistPage.tsx
@@ -1,24 +1,11 @@
-import { useNavigate } from 'react-router-dom';
-import { Header } from '@/components/common/Header/Header';
-import Icon from '@/components/common/Icon';
+import PollOptionSection from '@/components/vote-regist/PollOptionSection';
 import { PollProvider } from '@/components/vote-regist/Provider/PollRegistProvider';
 
 export default function VoteRegistPage() {
-  const navigate = useNavigate();
-
-  const handleClickBackButton = () => {
-    navigate(-1);
-  };
-
   return (
     <div>
-      <Header
-        leftNode={
-          <Icon name="ArrowLeft" size="large" onClick={handleClickBackButton} />
-        }
-      />
       <PollProvider>
-        <></>
+        <PollOptionSection />
       </PollProvider>
     </div>
   );

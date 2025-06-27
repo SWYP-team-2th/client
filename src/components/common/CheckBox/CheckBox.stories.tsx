5a8b829d360e3e5d2e8c294bf58081c16b6e25ad
정찬영 <q135794268020@gmail.com>
2025-06-27 12:31:41 +0900

    lint

--- src/components/common/CheckBox/CheckBox.stories.tsx
+++ src/components/common/CheckBox/CheckBox.stories.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import type { Meta, StoryObj } from '@storybook/react';
 import CheckBox from './CheckBox';
+import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof CheckBox> = {
   title: 'Common/CheckBox',

5a4db31825895f5e27be5b0c577f8de4e3217525
정찬영 <q135794268020@gmail.com>
2025-07-15 15:16:38 +0900

    상세페이지 사진 컴포넌트 구현

---
+++ src/components/poll-detail/Card/CardList.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import CardItem from '@/components/poll-detail/Card/CardItem';
+import { Post } from '@/types/post';
+
+export default function CardList({ post }: { post: Post }) {
+  const [checkedItems, setCheckedItems] = useState<{ [key: string]: boolean }>(
+    {},
+  );
+
+  console.log(checkedItems);
+
+  const handleCheck =
+    (id: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (post.pollOptions.pollType === 'SINGLE') {
+        // 단일 투표
+        setCheckedItems({ [id]: e.target.checked });
+      } else {
+        // 복수 투표
+        setCheckedItems((prev) => ({ ...prev, [id]: e.target.checked }));
+      }
+    };
+
+  return (
+    <div className="grid grid-cols-2 gap-x-5 gap-y-8 mx-[30px]">
+      {post.pollChoices.map((choice) => (
+        <CardItem
+          key={choice.id}
+          choice={choice}
+          checked={!!checkedItems[choice.id]}
+          onChange={handleCheck(String(choice.id))}
+        />
+      ))}
+    </div>
+  );
+}

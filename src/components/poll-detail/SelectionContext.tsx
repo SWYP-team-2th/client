6878e3d74b81cdd66b89e1f1a44f5f3c95439000
정찬영 <q135794268020@gmail.com>
2025-09-06 14:01:50 +0900

    개별 투표 관련 속성을 통합하여 post 객체로 변경

--- src/components/poll-detail/SelectionContext.tsx
+++ src/components/poll-detail/SelectionContext.tsx
@@ -20,29 +20,23 @@ const SelectionContext = createContext<SelectionContextValue | undefined>(
 );
 
 interface SelectionProviderProps extends PropsWithChildren {
-  pollType: Post['pollOption']['pollType'];
-  pollChoices: Post['pollChoices'];
-  isVoted: boolean;
+  post: Post;
 }
 
-export function SelectionProvider({
-  pollType,
-  pollChoices,
-  isVoted,
-  children,
-}: SelectionProviderProps) {
+export function SelectionProvider({ post, children }: SelectionProviderProps) {
   const [checkedItems, setCheckedItems] = useState<number[]>([]);
   const [voteMode, setVoteMode] = useState<boolean>(false);
 
+  const pollType = post.pollOption.pollType;
+
   // 사용자가 선택한 투표 사진들의 id만 뽑아서 체크 상태로 복원시키기
   useEffect(() => {
-    if (isVoted && !voteMode) {
-      const alreadyVote = pollChoices
+    setCheckedItems(
+      post.pollChoices
         .filter((choice) => choice.voteId !== null)
-        .map((choice) => choice.id);
-      setCheckedItems(alreadyVote);
-    }
-  }, [isVoted, pollChoices, voteMode]);
+        .map((choice) => choice.id),
+    );
+  }, [post]);
 
   const handleVoteChoice = (id: number, checked: boolean) => {
     setCheckedItems((prev) => {

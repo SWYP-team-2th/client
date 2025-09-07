349aefb032f3dab5cc0c8f5493ff01a61070b2b7
정찬영 <q135794268020@gmail.com>
2025-09-08 00:21:24 +0900

    useInfiniteQuery로 변경하여 페이지네이션 기능을 추가

--- src/api/useGetComments.ts
+++ src/api/useGetComments.ts
@@ -1,23 +1,25 @@
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { request } from './config';
 import { CommentsResponse } from '@/types/comment';
 
-export default function useGetComments(
-  postId: number,
-  size: number = 10,
-  options?: Partial<UseQueryOptions<CommentsResponse, Error>>,
-) {
-  return useQuery<CommentsResponse>({
-    queryFn: () =>
+export function useGetComments(postId: number, size: number = 10) {
+  return useInfiniteQuery<CommentsResponse>({
+    queryFn: ({ pageParam = null }) =>
       request({
         method: 'GET',
         url: `/posts/${postId}/comments`,
         params: {
-          cursor: null,
+          cursor: pageParam,
           size,
         },
       }),
     queryKey: ['comments', postId, size],
-    ...options,
+    initialPageParam: null,
+    getNextPageParam: (lastPage) => {
+      if (!lastPage.comments.hasNext || lastPage.comments.data.length === 0) {
+        return undefined;
+      }
+      return lastPage.comments.nextCursor;
+    },
   });
 }

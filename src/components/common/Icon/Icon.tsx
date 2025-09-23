e565bad630385418bc87cdc2006547a81c3d488d
정찬영 <q135794268020@gmail.com>
2025-09-23 16:25:46 +0900

    사용하지 않는 Icon svg 제거

--- src/components/common/Icon/Icon.tsx
+++ src/components/common/Icon/Icon.tsx
@@ -1,143 +1,74 @@
 import { SVGProps } from 'react';
-import AlertFill from '@/assets/icons/alert_fill_24px.svg?react';
-import AlertOutline from '@/assets/icons/alert_outline_24px.svg?react';
-import AlertV2 from '@/assets/icons/alert_V2.svg?react';
 import ArrowLeft from '@/assets/icons/arrow_left_24px.svg?react';
 import ArrowRight from '@/assets/icons/arrow_right_24px.svg?react';
-import ArrowRightPurple from '@/assets/icons/arrow_right_purple.svg?react';
-import BellFill from '@/assets/icons/bell_fill_24px.svg?react';
 import BellOutline from '@/assets/icons/bell_outline_24px.svg?react';
-import BookmarkFill from '@/assets/icons/bookmark_fill_24px.svg?react';
-import BookmarkOutline from '@/assets/icons/bookmark_outline_24px.svg?react';
-import Check from '@/assets/icons/check_24px.svg?react';
 import CheckLarge from '@/assets/icons/check_large.svg?react';
 import CheckSmall from '@/assets/icons/check_small.svg?react';
 import Close from '@/assets/icons/close.svg?react';
 import ThickClose from '@/assets/icons/close_thick.svg?react';
 import Cross from '@/assets/icons/cross_24px.svg?react';
-import CrossGray from '@/assets/icons/cross_gray.svg?react';
 import DeadLineDarkGray from '@/assets/icons/dead-Line-dark-gray.svg?react';
 import DeadLineGray from '@/assets/icons/dead-line-gray.svg?react';
 import DeadLine from '@/assets/icons/dead_line.svg?react';
 import Edit from '@/assets/icons/edit_24px.svg?react';
-import Filter from '@/assets/icons/filter.svg?react';
 import FullPhoto from '@/assets/icons/full.svg?react';
-import HeartFill from '@/assets/icons/heart_fill_24px.svg?react';
-import HeartFillRed from '@/assets/icons/heart_fill_Red.svg?react';
-import HeartOutline from '@/assets/icons/heart_outline_24px.svg?react';
-import HeartOutlineWhite from '@/assets/icons/heart_outline_white.svg?react';
 import HomeFilled from '@/assets/icons/home-filled.svg?react';
 import HomeOutline from '@/assets/icons/home.svg?react';
 import KakaoLogo from '@/assets/icons/kako_logo.svg?react';
-import Link from '@/assets/icons/link_24px.svg?react';
 import LinkWhite from '@/assets/icons/link_white.svg?react';
-import LockFill from '@/assets/icons/lock_fill_24px.svg?react';
-import LockOpen from '@/assets/icons/lock_open_24px.svg?react';
-import LockOutline from '@/assets/icons/lock_outline_24px.svg?react';
 import Logo from '@/assets/icons/logo.svg?react';
-import Logout from '@/assets/icons/logout.svg?react';
 import LogoutGray from '@/assets/icons/logout_gray.svg?react';
-import Maximize from '@/assets/icons/maximize_24px.svg?react';
-import Medal from '@/assets/icons/medal.svg?react';
-import MedalWhite from '@/assets/icons/medal_fill_white.svg?react';
 import Menu from '@/assets/icons/menu_24px.svg?react';
 import MessageOutlineDarkGray from '@/assets/icons/message-outline-dark-gray.svg?react';
-import MessageOutlineGray from '@/assets/icons/message-outline-gray.svg?react';
-import MessageFill from '@/assets/icons/message_fill_24px.svg?react';
 import MessageOutline from '@/assets/icons/message_outline_24px.svg?react';
-import Minimize from '@/assets/icons/minimize_24px.svg?react';
-import Minus from '@/assets/icons/minus_24px.svg?react';
 import More from '@/assets/icons/more_24px.svg?react';
 import PencilGray from '@/assets/icons/pencil-gray.svg?react';
-import PhotoPlus from '@/assets/icons/photo_plus_24px.svg?react';
 import PhotoPlusWhite from '@/assets/icons/photo_plus_gray.svg?react';
 import Plus from '@/assets/icons/plus_24px.svg?react';
-import PlusWhite from '@/assets/icons/plus_white.svg?react';
 import Post from '@/assets/icons/post_24px.svg?react';
 import PostWhite from '@/assets/icons/post_white.svg?react';
 import ArrowRightGray from '@/assets/icons/right_arrow_gray.svg?react';
-import SearchFill from '@/assets/icons/search_fill_24px.svg?react';
 import SendGray from '@/assets/icons/send_2.svg?react';
-import Send from '@/assets/icons/send_24px.svg?react';
 import SettingsOutline from '@/assets/icons/settings_outline_24px.svg?react';
-import Talk from '@/assets/icons/talk.svg?react';
-import ThumbUpFill from '@/assets/icons/thumb-up_fill_24px.svg?react';
-import ThumbUpOutline from '@/assets/icons/thumb-up_outline_24px.svg?react';
 import ThumbUpOutlineGray from '@/assets/icons/thumbs_up.svg?react';
 import ThumbUpFillGray from '@/assets/icons/thumbs_up_filled.svg?react';
 import ToastError from '@/assets/icons/toast_error.svg?react';
 import ToastInfo from '@/assets/icons/toast_info.svg?react';
 import ToastSuccess from '@/assets/icons/toast_success.svg?react';
 import ToastWarning from '@/assets/icons/toast_warning.svg?react';
-import TrashCanGray from '@/assets/icons/trash-can-gray.svg?react';
 import Trash from '@/assets/icons/trash_24px.svg?react';
 import UpLoad from '@/assets/icons/upload.svg?react';
 import User2Fill from '@/assets/icons/user2_fill_24px.svg?react';
 import User2Outline from '@/assets/icons/user2_outline_24px.svg?react';
-import UserFill from '@/assets/icons/user_fill_24px.svg?react';
-import UserOutline from '@/assets/icons/user_outline_24px.svg?react';
-import VoteGray from '@/assets/icons/vote-gray.svg?react';
-import Vote from '@/assets/icons/vote.svg?react';
-import VoteClose from '@/assets/icons/vote_close.svg?react';
-import World from '@/assets/icons/world_24px.svg?react';
 
 export const ICONS = {
   DeadLineGray,
   UpLoad,
-  CrossGray,
   CheckLarge,
   CheckSmall,
   LogoutGray,
   SendGray,
   ArrowRightGray,
-  VoteGray,
-  MessageOutlineGray,
   MessageOutlineDarkGray,
   DeadLineDarkGray,
   PostWhite,
-  Vote,
-  Talk,
   HomeFilled,
-  Filter,
-  ArrowRightPurple,
   PhotoPlusWhite,
-  AlertFill,
-  AlertOutline,
-  AlertV2,
   ArrowLeft,
   ArrowRight,
-  BellFill,
   BellOutline,
   DeadLine,
-  BookmarkFill,
-  BookmarkOutline,
-  Check,
   Close,
   ThickClose,
   Cross,
   Edit,
-  HeartFill,
-  HeartFillRed,
-  HeartOutline,
   HomeOutline,
-  Link,
-  LockFill,
-  LockOpen,
-  LockOutline,
-  Maximize,
   Menu,
-  MessageFill,
   MessageOutline,
-  Minimize,
-  Minus,
   More,
-  PhotoPlus,
   Plus,
   Post,
-  SearchFill,
   SettingsOutline,
-  ThumbUpFill,
-  ThumbUpOutline,
   ToastError,
   ToastInfo,
   ToastSuccess,
@@ -145,20 +76,9 @@ export const ICONS = {
   Trash,
   User2Fill,
   User2Outline,
-  UserFill,
-  UserOutline,
-  VoteClose,
-  World,
   Logo,
-  Logout,
-  Send,
-  Medal,
-  MedalWhite,
-  PlusWhite,
-  HeartOutlineWhite,
   KakaoLogo,
   PencilGray,
-  TrashCanGray,
   LinkWhite,
   FullPhoto,
   ThumbUpFillGray,

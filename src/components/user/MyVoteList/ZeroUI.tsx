cb99e062c29f4059f1e0afc0730fa222af18d268
YOOJS1205 <junsang8921@gmail.com>
2025-09-30 22:27:55 +0900

    empty state 아이콘 및 텍스트 크기 수정

--- src/components/user/MyVoteList/ZeroUI.tsx
+++ src/components/user/MyVoteList/ZeroUI.tsx
@@ -4,15 +4,15 @@ export default function ZeroUI() {
   return (
     <div className="text-center text-gray-500 flex flex-col items-center justify-center h-full min-h-[400px]">
       <img
-        className="w-[188px] h-[188px]"
+        className="w-[100px] h-[100px]"
         src={EmptyMyVoteList}
         alt="아직 만든 투표가 없어요!"
       />
       <div className="flex flex-col gap-[6px]">
-        <span className="text-gray-900 text-heading-2">
+        <span className="text-gray-900 text-headline-1">
           아직 만든 투표가 없어요!
         </span>
-        <span className="text-gray-600 text-headline-3">
+        <span className="text-gray-600 text-body-1">
           첫 투표를 만들어 사람들의 선택을 받아보세요.
         </span>
       </div>

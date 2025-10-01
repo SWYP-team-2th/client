c5ece6a8456ac8013c36eed55ee9e0e18c9781b5
유준상 (1팀 프론트) <89122773+YOOJS1205@users.noreply.github.com>
2025-10-01 14:12:26 +0900

    empty state 아이콘 및 텍스트 크기 수정 (#433)

--- src/components/user/ParticipatedVoteList/ZeroUI.tsx
+++ src/components/user/ParticipatedVoteList/ZeroUI.tsx
@@ -4,15 +4,15 @@ export default function ZeroUI() {
   return (
     <div className="text-center text-gray-500 flex flex-col items-center justify-center h-full min-h-[400px]">
       <img
-        className="w-[188px] h-[188px]"
+        className="w-[100px] h-[100px]"
         src={EmptyParticipatedVoteList}
         alt="아직 참여한 투표가 없어요!"
       />
       <div className="flex flex-col gap-[6px]">
-        <span className="text-gray-900 text-heading-2">
+        <span className="text-gray-900 text-headline-1">
           아직 참여한 투표가 없어요!
         </span>
-        <span className="text-gray-600 text-headline-3">
+        <span className="text-gray-600 text-body-1">
           다른 사람들의 투표에 참여하고, 결과도 확인해보세요.
         </span>
       </div>

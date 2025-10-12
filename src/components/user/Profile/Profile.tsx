4cd9809d3feddbb3c2299e5daf3ec022c1a8ebe8
유준상 (1팀 프론트) <89122773+YOOJS1205@users.noreply.github.com>
2025-10-12 21:02:35 +0900

    마이페이지 스타일을 전반적으로 수정한다. (#447)

--- src/components/user/Profile/Profile.tsx
+++ src/components/user/Profile/Profile.tsx
@@ -12,17 +12,17 @@ export default function Profile({ profileImageUrl, name }: ProfileProps) {
 
   return (
     <div className="px-5 pt-6 pb-12 bg-primary-500 text-gray-100 flex justify-between items-center">
-      <div className="flex gap-4">
+      <div className="flex gap-4 items-center">
         <Avatar size="large" src={profileImageUrl} />
-        <p className="text-title-3">{name}</p>
+        <p className="text-heading-2">{name}</p>
       </div>
       {isMyProfile && (
         <button
           className="flex items-center gap-[2px] bg-primary-600 py-1 px-2 rounded-full cursor-pointer"
           onClick={handleClickSettingsButton}
         >
-          <Icon name="SettingsOutline" size="medium" strokeColor="white" />
-          <p className="text-body-1">설정</p>
+          <Icon name="SettingsOutline" size="small" strokeColor="white" />
+          <p className="text-body-2">설정</p>
         </button>
       )}
     </div>

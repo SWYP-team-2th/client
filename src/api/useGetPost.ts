ed41476e7289f85ef74fdba1ecf2881185e70d22
유준상 (1팀 프론트) <89122773+YOOJS1205@users.noreply.github.com>
2025-10-27 00:01:50 +0900

    비공개 게시물에 대해서 shareKey query를 실어서 요청하도록 수정한다. (#461)

--- src/api/useGetPost.ts
+++ src/api/useGetPost.ts
@@ -2,13 +2,19 @@ import { useQuery } from '@tanstack/react-query';
 import type { Post } from '@/types/post';
 import { request } from '@/api/config';
 
-export const useGetPost = (postId: string) => {
+interface UseGetPostParams {
+  postId: string;
+  shareKey?: string;
+}
+
+export const useGetPost = ({ postId, shareKey }: UseGetPostParams) => {
   return useQuery({
-    queryKey: ['post', postId],
+    queryKey: ['post', postId, shareKey],
     queryFn: () =>
       request<Post>({
         method: 'GET',
         url: `/posts/${postId}`,
+        params: { shareKey },
       }),
     enabled: !!postId,
   });

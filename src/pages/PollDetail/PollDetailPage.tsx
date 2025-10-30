2b4778053c8fcd9723b2c662f9e1082fe1d09530
정찬영 <q135794268020@gmail.com>
2025-10-30 20:34:36 +0900

    본인 투표이면 투표결과가 항상 보이도록 한다. (#465)

--- src/pages/PollDetail/PollDetailPage.tsx
+++ src/pages/PollDetail/PollDetailPage.tsx
@@ -25,7 +25,11 @@ export default function PollDetailPage() {
     return <NotFoundPage />;
   }
 
+
+  const showResultList = post.isAuthor || isVoted || post.status === 'CLOSED';
+
   const shareUrl = `${window.location.origin}/posts/${postId}?shareUrl=${post.shareUrl}`;
+
 
   return (
     <div className="w-full bg-gray-100 h-screen flex itmes-center flex-col pt-[60px] relative">
@@ -67,7 +71,7 @@ export default function PollDetailPage() {
       />
 
       {/* 투표 결과 섹션 */}
-      {(isVoted || post.status === 'CLOSED') && result && (
+      {showResultList && result && (
         <div className="px-5 border-y-[1px] border-gray-200 mb-[30px]">
           <div className="text-headline-1 text-gray-800 mt-[35px]">
             투표 결과

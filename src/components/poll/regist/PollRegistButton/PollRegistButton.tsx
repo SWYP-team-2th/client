1b45c8b45921d18dd7c1e902f046b90eec86810f
유준상 (1팀 프론트) <89122773+YOOJS1205@users.noreply.github.com>
2025-12-20 01:31:32 +0900

    첫 투표 생성 시 ga 이벤트를 발생시킨다. (#490)

--- src/components/poll/regist/PollRegistButton/PollRegistButton.tsx
+++ src/components/poll/regist/PollRegistButton/PollRegistButton.tsx
@@ -1,3 +1,4 @@
+import ReactGA from 'react-ga4';
 import { useNavigate } from 'react-router-dom';
 import usePollForm from '../../Provider/hooks';
 import useGetMyInfo from '@/api/useGetMyInfo';
@@ -18,6 +19,10 @@ export default function PollRegistButton() {
     usePostRegistVote({
       onSuccess: (data) => {
         if (myInfo?.onboardingStep) {
+          ReactGA.event('poll_created', {
+            post_id: data.postId,
+          });
+
           updateOnboarding({
             onboardingStep: { ...myInfo.onboardingStep, FIRST_VOTE: false },
           });

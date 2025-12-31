99149a0394d92929f0fa3d6544c0335e3a8cb4ac
정찬영 <q135794268020@gmail.com>
2025-12-31 09:01:23 +0900

    lint 에러 수정

--- src/components/poll/regist/PollRegistButton/PollRegistButton.tsx
+++ src/components/poll/regist/PollRegistButton/PollRegistButton.tsx
@@ -1,14 +1,14 @@
 import ReactGA from 'react-ga4';
 import { useNavigate } from 'react-router-dom';
-import usePollForm from '../../Provider/hooks';
+import useGetMyInfo from '@/api/useGetMyInfo';
+import usePostRegistVote from '@/api/usePostRegistVote';
+import useUpdateOnboarding from '@/api/useUpdateOnboarding';
 import { useBottomSheet } from '@/components/common/BottomSheet/hooks';
 import { Button } from '@/components/common/Button/Button';
 import Loading from '@/components/common/Loading';
 import PollCreatedShareBottomSheet from '@/components/common/PollCreatedShareBottomSheet';
-import useGetMyInfo from '@/api/useGetMyInfo';
-import usePostRegistVote from '@/api/usePostRegistVote';
-import useUpdateOnboarding from '@/api/useUpdateOnboarding';
 import { useModerationCheck } from '@/hooks/useModerationCheck';
+import usePollForm from '../../Provider/hooks';
 
 export default function PollRegistButton() {
   const navigate = useNavigate();

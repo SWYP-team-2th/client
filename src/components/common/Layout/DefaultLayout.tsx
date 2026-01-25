19203baee262b3a78f60353d42415015c7013291
oyuns-wishket <oyuns@wishket.com>
2026-01-25 19:31:48 +0900

    feat : ga4 이벤트 추가 및 spa페이지뷰 트래킹

--- src/components/common/Layout/DefaultLayout.tsx
+++ src/components/common/Layout/DefaultLayout.tsx
@@ -1,7 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import Navigation from '@/components/common/Navigation/Navigation';
+import usePageViewTracking from '@/hooks/usePageViewTracking';
 
 export default function DefaultLayout() {
+  usePageViewTracking();
+
   return (
     <div className="relative w-full h-full mx-auto my-0 min-h-lvh max-w-[480px]">
       <div className="pb-[80px]">
